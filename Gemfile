4acbd3bb1ea0565c6eb6485a366fb28531b6b5ed
Srinivas Aki <srinivas.aki@gmail.com>
2011-06-19 05:05:57 -0700

    Processing images to 100x100

--- Gemfile
+++ Gemfile
@@ -7,4 +7,4 @@ gem "bson_ext", "~> 1.3.1"
 gem "mongoid_geo", "~> 0.6.0"
 gem "carrierwave"
 gem "fog"
-
+gem "mini_magick"
--- image_uploader.rb
+++ image_uploader.rb
@@ -1,7 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
   storage :fog
 
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+
+
+  process :resize_to_fill => [100, 100]
 end
